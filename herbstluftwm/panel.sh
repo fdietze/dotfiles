090a67d93a07758a09cd20c9db47b4cb3deb1271
Felix Dietze <felix@beerleader.net>
2014-03-14 03:45:21 +0100

    use conky for stats

--- herbstluftwm/panel.sh
+++ herbstluftwm/panel.sh
@@ -4,7 +4,7 @@ hc() { "${herbstclient_command[@]:-herbstclient}" "$@" ;}
 monitor=${1:-0}
 geometry=( $(herbstclient monitor_rect "$monitor") )
 if [ -z "$geometry" ] ;then
-    echo "Invalid monitor $monitor"
+echo "Invalid monitor $monitor"
     exit 1
 fi
 # geometry has the format W H X Y
@@ -12,178 +12,64 @@ x=${geometry[0]}
 y=${geometry[1]}
 panel_width=${geometry[2]}
 panel_height=22
+
 font="-*-fixed-medium-*-*-*-14-*-*-*-*-*-*-*"
-bgcolor=#121212
-selbg=$(hc get window_border_active_color)
-selfg='#101010'
-
-####
-# Try to find textwidth binary.
-# In e.g. Ubuntu, this is named dzen2-textwidth.
-if which textwidth &> /dev/null ; then
-    textwidth="textwidth";
-elif which dzen2-textwidth &> /dev/null ; then
-    textwidth="dzen2-textwidth";
-else
-    echo "This script requires the textwidth tool of the dzen2 project."
-    exit 1
-fi
-####
-# true if we are using the svn version of dzen2
-# depending on version/distribution, this seems to have version strings like
-# "dzen-" or "dzen-x.x.x-svn"
-if dzen2 -v 2>&1 | head -n 1 | grep -q '^dzen-\([^,]*-svn\|\),'; then
-    dzen2_svn="true"
-else
-    dzen2_svn=""
-fi
-
-if awk -Wv 2>/dev/null | head -1 | grep -q '^mawk'; then
-    # mawk needs "-W interactive" to line-buffer stdout correctly
-    # http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=593504
-    uniq_linebuffered() {
-      awk -W interactive '$0 != l { print ; l=$0 ; fflush(); }' "$@"
-    }
-else
-    # other awk versions (e.g. gawk) issue a warning with "-W interactive", so
-    # we don't want to use it there.
-    uniq_linebuffered() {
-      awk '$0 != l { print ; l=$0 ; fflush(); }' "$@"
-    }
-fi
+bgcolor='#121212'
+fgcolor='#efefef'
+bgcolorsel='#37BAFF'
+fgcolorsel='#101010'
 
 hc pad $monitor $panel_height
 
+function uniq_linebuffered() {
+   awk '$0 != l { print ; l=$0 ; fflush(); }' "$@"
+}
+ 
 {
-    ### Event generator ###
-    # based on different input data (mpc, date, hlwm hooks, ...) this generates events, formed like this:
-    #   <eventname>\t<data> [...]
-    # e.g.
-    #   date    ^fg(#efefef)18:33^fg(#909090), 2013-10-^fg(#efefef)29
-
-    #mpc idleloop player &
-    while true ; do
-        # "date" output is checked once a second, but an event is only
-        # generated if the output changed compared to the previous run.
-        date +$'date\t^fg(#909090)%Y-%m-^fg(#efefef)%d, %H:%M'
-        sleep 1 || break
-    done > >(uniq_linebuffered) &
-    childpid=$!
-    hc --idle
-    kill $childpid
-} 2> /dev/null | {
-    IFS=$'\t' read -ra tags <<< "$(hc tag_status $monitor)"
-    visible=true
-    date=""
-    windowtitle=""
-    while true ; do
-
-        ### Output ###
-        # This part prints dzen data based on the _previous_ data handling run,
-        # and then waits for the next event to happen.
-
-        bordercolor="#26221Cd"
-        separator="^bg()^fg($selbg)|"
-        # draw tags
-        for i in "${tags[@]}" ; do
+   conky -c ~/.config/herbstluftwm/conkybar | while read -r; do
+      echo -e "conky $REPLY";
+     
+   done > >(uniq_linebuffered) &
+   childpid=$!
+   herbstclient --idle
+   kill $childpid
+} | {
+   TAGS=( $(herbstclient tag_status $monitor) )
+      conky=""
+      separator="^fg($bgcolorsel)^ro(1x16)^fg()"
+      while true; do
+         for i in "${TAGS[@]}"; do
+            echo -n "^ca(1,herbstclient use ${i:1}) "
             case ${i:0:1} in
-                '#')
-                    echo -n "^bg($selbg)^fg($selfg)"
-                    ;;
-                '+')
-                    echo -n "^bg(#9CA668)^fg(#141414)"
-                    ;;
-                ':')
-                    echo -n "^bg()^fg(#ffffff)"
-                    ;;
-                '!')
-                    echo -n "^bg(#FF0675)^fg(#141414)"
-                    ;;
-                *)
-                    echo -n "^bg()^fg(#ababab)"
-                    ;;
+            '#')
+                echo -n "^bg($bgcolorsel) ^fg($fgcolorsel)${i:1}^fg($fgcolor) ^bg($bgcolor)"
+               ;;
+            ':')
+               echo -n "^fg(#CCCCCC) ${i:1} "
+               ;;
+            *)
+               echo -n "^fg(#444444) ${i:1} "
+               ;;
             esac
-            if [ ! -z "$dzen2_svn" ] ; then
-                # clickable tags if using SVN dzen
-                echo -n "^ca(1,\"${herbstclient_command[@]:-herbstclient}\" "
-                echo -n "focus_monitor \"$monitor\" && "
-                echo -n "\"${herbstclient_command[@]:-herbstclient}\" "
-                echo -n "use \"${i:1}\") ${i:1} ^ca()"
-            else
-                # non-clickable tags if using older dzen
-                echo -n " ${i:1} "
-            fi
+            echo -n "^ca()"
         done
-        echo -n "$separator"
-        echo -n "^bg()^fg() ${windowtitle//^/^^}"
-        # small adjustments
-        right="$separator^bg() $date"
-        right_text_only=$(echo -n "$right" | sed 's.\^[^(]*([^)]*)..g')
-        # get width of right aligned text.. and add some space..
-        width=$($textwidth "$font" "$right_text_only    ")
-        echo -n "^pa($(($panel_width - $width)))$right"
+        echo -n " $separator "
+        echo -n `herbstclient attr clients.focus.title`
+        conky_text_only=$(echo -n "$conky "|sed 's.\^[^(]*([^)]*)..g')
+        width=$(textwidth "$font" "$conky_text_only  ")
+        echo -n "^p(_RIGHT)^p(-$width)$conky"
         echo
-
-        ### Data handling ###
-        # This part handles the events generated in the event loop, and sets
-        # internal variables based on them. The event and its arguments are
-        # read into the array cmd, then action is taken depending on the event
-        # name.
-        # "Special" events (quit_panel/togglehidepanel/reload) are also handled
-        # here.
-
-        # wait for next event
-        IFS=$'\t' read -ra cmd || break
-        # find out event origin
-        case "${cmd[0]}" in
-            tag*)
-                #echo "resetting tags" >&2
-                IFS=$'\t' read -ra tags <<< "$(hc tag_status $monitor)"
-                ;;
-            date)
-                #echo "resetting date" >&2
-                date="${cmd[@]:1}"
-                ;;
-            quit_panel)
-                exit
-                ;;
-            togglehidepanel)
-                currentmonidx=$(hc list_monitors | sed -n '/\[FOCUS\]$/s/:.*//p')
-                if [ "${cmd[1]}" -ne "$monitor" ] ; then
-                    continue
-                fi
-                if [ "${cmd[1]}" = "current" ] && [ "$currentmonidx" -ne "$monitor" ] ; then
-                    continue
-                fi
-                echo "^togglehide()"
-                if $visible ; then
-                    visible=false
-                    hc pad $monitor 0
-                else
-                    visible=true
-                    hc pad $monitor $panel_height
-                fi
-                ;;
-            reload)
-                exit
-                ;;
-            focus_changed|window_title_changed)
-                windowtitle="${cmd[@]:2}"
-                ;;
-            #player)
-            #    ;;
-        esac
-    done
-
-    ### dzen2 ###
-    # After the data is gathered and processed, the output of the previous block
-    # gets piped to dzen2.
-
+        read line || break
+        cmd=( $line )
+   case "$cmd[0]" in
+               tag*)
+                  TAGS=( $(herbstclient tag_status $monitor) )
+                  ;;
+               conky*)
+                  conky="${cmd[@]:1}"
+                  ;;
+               esac
+     done
 } 2> /dev/null | dzen2 -w $panel_width -x $x -y $y -fn "$font" -h $panel_height \
-    -e 'button3=' \
-    -ta l -bg "$bgcolor" -fg '#efefef'
-
-
-trayer --edge top --align right --widthtype request --heighttype pixel \
-           --height 22 --expand true --tint 0x222222 --transparent true \
-                  --alpha 1
+              -e 'button3=' \
+              -ta l -bg "$bgcolor" -fg $fgcolor
