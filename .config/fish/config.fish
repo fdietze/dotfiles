f3fdc2b941e73839d3060ebd5306c7dc74669db5
Felix Dietze <felix@beerleader.net>
2014-10-02 16:45:17 +0200

    configure fish-shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,55 @@
+# parse .profile
+egrep "^export " ~/.profile | while read e
+	set var (echo $e | sed -E "s/^export ([A-Z_]+)=(.*)\$/\1/")
+	set value (echo $e | sed -E "s/^export ([A-Z_]+)=(.*)\$/\2/")
+	
+	# remove surrounding quotes if existing
+	set value (echo $value | sed -E "s/^\"(.*)\"\$/\1/")
+
+	if test $var = "PATH"
+		# replace ":" by spaces. this is how PATH looks for Fish
+		set value (echo $value | sed -E "s/:/ /g")
+	
+		# use eval because we need to expand the value
+		eval set -xg $var $value
+
+		continue
+	end
+
+	# evaluate variables. we can use eval because we most likely just used "$var"
+	set value (eval echo $value)
+
+	#echo "set -xg '$var' '$value' (via '$e')"
+	set -xg $var $value
+end
+
+
+set fish_greeting ""
+
+
+source ~/.sh_aliases
+
+# activate z
+. ~/bin/z.fish
+
+# colorize a huge set of commands
+set -x PATH (cope_path) $PATH
+
+# activate colorful file listings
+eval "set -x LS_COLORS" (dircolors ~/.dir_colors | grep -P "'.*'" -o)
+
+# colorize manpages
+set -x LESS_TERMCAP_mb (printf "\33[01;34m") # begin blinking
+set -x LESS_TERMCAP_md (printf "\33[01;34m") # begin bold
+set -x LESS_TERMCAP_me (printf "\33[0m")     # end mode
+set -x LESS_TERMCAP_se (printf "\33[0m")     # end standout-mode
+set -x LESS_TERMCAP_so (printf "\33[30;46m") # begin standout-mode - info box
+set -x LESS_TERMCAP_ue (printf "\33[0m")     # end underline
+set -x LESS_TERMCAP_us (printf "\33[01;35m") # begin underline
+
+# start X at login
+if status --is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR = 1
+        # exec startx
+    end
+end
--- .profile
+++ .profile
@@ -1,11 +1,17 @@
 export _JAVA_AWT_WM_NONREPARENTING=1
 #JAVA_HOME=/opt/java7
-PATH=$HOME/bin:$PATH
-PATH=$HOME/local/play:$PATH
-PATH=$HOME/local/activator:$PATH
+export PATH=$HOME/bin:$PATH
+export PATH=/home/felix/local/android-studio/sdk/tools:$PATH
+export PATH=/home/felix/local/android-studio/sdk/platform-tools:$PATH
 export GOPATH=~/go
+
 export BROWSER=chromium
+export EDITOR=vim
+export DE=mate
+
 
 # syntax highlighting for less
-export LESSOPEN="| /usr/bin/src-hilite-lesspipe.sh %s"
+# export LESSOPEN="| /usr/bin/src-hilite-lesspipe.sh %s" # package: source-highlight
+export LESSOPEN="| highlight %s -O ansi" # package: highlight-gui
 export LESS=' -R '
+
