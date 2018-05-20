36bd546497265508c6e3ab9821eb2118bdbaf9ae
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-05-20 21:21:48 +0200

    better human_duration

--- bin/human-duration.py
+++ bin/human-duration.py
@@ -3,4 +3,13 @@
 import sys
 from isodate import parse_duration
 
-print(parse_duration(sys.stdin.read()))
+duration = parse_duration(sys.stdin.read())
+
+hours, remainder = divmod(duration.total_seconds(), 3600)
+minutes, seconds = divmod(remainder, 60)
+if hours > 0:
+    print('%dh %dm' % (hours, minutes))
+elif minutes > 0: 
+    print('%dm' % (minutes))
+else: 
+    print('%ds' % (seconds))
