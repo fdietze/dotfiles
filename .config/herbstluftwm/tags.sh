81d1f069b3c8bc98a0e6e5f9ec10e5c3be7bea11
Felix Dietze <felix.dietze@rwth-aachen.de>
2020-07-15 20:43:11 +0200

    Back to herbstluftwm

--- .config/herbstluftwm/tags.sh
+++ .config/herbstluftwm/tags.sh
@@ -1,42 +1,65 @@
 #!/usr/bin/env bash
 hc() { "${herbstclient_command[@]:-herbstclient}" "$@" ;}
-monitor=0
+monitor=$1
+
+THEME=$(cat $HOME/.theme || echo "light")
 
 
-bgcolor="#121212"
-fgcolor="#EFEFEF"
-fgcolorinact="#444444"
-fgcolordim="#909090"
-fgcolorbad="#FF3F74"
-bgcolorsel="#37BAFF"
-fgcolorsel="#000000"
-bgcolorurgent="#CE6D00"
+# case $THEME in
+    # light)
+        # ;;
+        default_bg="#ffffff"
+        used_fg="#555555"
+        empty_fg="#CCCCCC"
+        fgcolordim="#909090"
+        fgcolorbad="#FF3F74"
+        bgcolorsel="#5A5B66"
+        fgcolorsel="#ffffff"
+        bgcolorurgent="#FFD8AC"
+    # dark)
+        # default_bg="#ffffff"
+        # used_fg="#EFEFEF"
+        # empty_fg="#444444"
+        # fgcolordim="#909090"
+        # fgcolorbad="#FF3F74"
+        # bgcolorsel="#37BAFF"
+        # fgcolorsel="#000000"
+        # bgcolorurgent="#CE6D00"
+        # ;;
+# esac
 
 tag_status() {
     IFS=$'\t' read -ra tags <<< "$(hc tag_status $monitor)"
 
+
     for i in "${tags[@]}" ; do
         case ${i:0:1} in
-            '#')
+            ':') # the tag is used (not empty).
+                echo -n "%{B$default_bg F$used_fg}"
+                ;;
+            '+') # the tag is viewed on the specified MONITOR, but this monitor is not focused.
+                echo -n "%{B#A2A4B8 F$fgcolorsel}"
+                ;;
+            '#') # the tag is viewed on the specified MONITOR and it is focused.
                 echo -n "%{B$bgcolorsel F$fgcolorsel}"
                 ;;
-            # '+')
-            #     echo -n "%{B#9CA668 F#141414}"
-            #     ;;
-            ':')
-                echo -n "%{B$bgcolor F$fgcolor}"
+            '-') # the tag is viewed on a different MONITOR, but this monitor is not focused.
+                echo -n "%{B#BDBFD6 F#$used_fg}"
                 ;;
-            '!')
-                echo -n "%{B$bgcolorurgent F$fgcolorsel}"
+            '%') # the tag is viewed on a different MONITOR and it is focused.
+                echo -n "%{B#DFE1FC F$used_fg}"
+                ;;
+            '!') # the tag contains an urgent window
+                echo -n "%{B$bgcolorurgent F$used_fg}"
                 ;;
             *)
-                echo -n "%{B$bgcolor F$fgcolorinact}"
+                echo -n "%{B$default_bg F$empty_fg}"
                 ;;
         esac
         echo -n " ${i:1} "
     done
-    echo "%{O0}" # for showing last space
-    echo -n "%{B$bgcolor F$fgcolor}" # reset colors
+    # echo "%{O0}" # for showing last space
+    echo -n "%{B$default_bg F$default_fg}" # reset colors
 }
 
 {
