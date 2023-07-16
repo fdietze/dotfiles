e4750473053b5ed1b827092c7739dc3ff0cf410f
Felix Dietze <felix.dietze@rwth-aachen.de>
2023-07-16 22:29:14 +0200

    Squashed '.config/nvim/lua/user/' content from commit 59b1209
    git-subtree-dir: .config/nvim/lua/user
    git-subtree-split: 59b12098bdb185ddd595f1bf356a1b986f860859

---
+++ plugins/treesitter.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    -- add more things to the ensure_installed table protecting against community packs modifying it
+    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+      -- "lua"
+    })
+  end,
+}
