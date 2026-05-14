95bfa8bbfce0dd7688ae330440b81d8358038690
Felix Dietze <github@felx.me>
2026-05-14 22:35:04 +0530

    Fix i3lock wallpaper path

--- modules/nixos/desktops/herbstluftwm.nix
+++ modules/nixos/desktops/herbstluftwm.nix
@@ -38,11 +38,12 @@ lib.mkIf (config.my.desktop == "herbstluftwm") {
     enable = true;
     # i3lock(1) recommends this xss-lock pattern so suspend waits until the screen is locked.
     extraOptions = [ "--transfer-sleep-lock" ];
+    # xss-lock does not run the locker through a shell; systemd expands %h for this user service.
     lockerCommand = ''
       ${config.programs.i3lock.package}/bin/i3lock-color \
             --nofork \
             --ignore-empty-password \
-            --image="$HOME/.cache/frottage/current-wallpaper.jpg" \
+            --image=%h/.cache/frottage/current-wallpaper.jpg \
             --ring-width=10 --line-uses-inside \
             --ring-color=222436FF   --ringver-color=C3E88DFF   --ringwrong-color=C53B53FF \
             --inside-color=000000AA --insidever-color=000000AA --insidewrong-color=000000AA \
