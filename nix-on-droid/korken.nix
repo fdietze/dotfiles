524a59aa0332b873a30f13122059b47fbd765911
Felix Dietze <github@felx.me>
2026-06-08 15:42:02 +0400

    feat(korken): add nix-on-droid module

---
+++ nix-on-droid/korken.nix
@@ -0,0 +1,30 @@
+{
+  pkgs,
+  ...
+}: {
+  # Nix-on-Droid keeps Android's runtime hostname as "localhost"; the stable
+  # repository identifier for this device is the flake output name "korken".
+  user = {
+    userName = "felix";
+    shell = "${pkgs.zsh}/bin/zsh";
+  };
+
+  # Nix-on-Droid's option reference uses nix.extraOptions for nix.conf text.
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  home-manager = {
+    backupFileExtension = "hm-bak";
+    useGlobalPkgs = true;
+    config = {...}: {
+      imports = [
+        ../modules/home-manager/profiles/shell-core.nix
+        ../modules/home-manager/profiles/standalone-extras.nix
+      ];
+    };
+  };
+
+  # Latest stateVersion listed in the current Nix-on-Droid option reference.
+  system.stateVersion = "24.05";
+}
