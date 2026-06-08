74f28cbccf9d196bf6f746cc6d10ecabc43c5b44
Felix Dietze <github@felx.me>
2026-06-08 14:05:20 +0400

    feat(gurke): explicitly activate NVF Neovim configuration

--- hosts/gurke/home.nix
+++ hosts/gurke/home.nix
@@ -5,5 +5,7 @@
     ../../modules/home-manager/desktops/gnome.nix
     ../../modules/home-manager/desktops/herbstluftwm.nix
     ../../modules/home-manager/desktops/noctalia-niri.nix
+    # NVF Neovim configuration explicitly enabled for this host.
+    ../../modules/home-manager/nvf.nix
   ];
 }
